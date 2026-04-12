7b4aa6ee75516484cd9e5272f64068a538a8d093
DanieleSavino <daniele.savino0@gmail.com>
2026-04-12 13:36:48 +0200

    Improved error handling

--- bench/src/cuda/reduce.cu
+++ bench/src/cuda/reduce.cu
@@ -1,5 +1,6 @@
 #include "bench/cuda/reduce.h"
 #include "CollAlgo/reduce.h"
+#include "CollAlgo/utils.h"
 #include <mpi.h>
 
 #define BUFF_SIZE 100000
@@ -17,21 +18,20 @@ static __global__ void check_kernel(int *buff, int size, int n, int *errors) {
 }
 
 void CA_bench_reduce_fill_cuda(int *buff, int rank, int n) {
-        int threads = 256;
-        int blocks  = (n + threads - 1) / threads;
-
-        fill_kernel<<<blocks, threads>>>(buff, rank, n);
+    int threads = 256;
+    int blocks  = (n + threads - 1) / threads;
+    fill_kernel<<<blocks, threads>>>(buff, rank, n);
+    CA_CUDA_CHECK(cudaGetLastError());
 }
 
 void CA_bench_reduce_check_cuda(int *buff, int size, int n, int *errors) {
-        int threads = 256;
-        int blocks  = (n + threads - 1) / threads;
-
-        check_kernel<<<blocks, threads>>>(buff, size, n, errors);
+    int threads = 256;
+    int blocks  = (n + threads - 1) / threads;
+    check_kernel<<<blocks, threads>>>(buff, size, n, errors);
+    CA_CUDA_CHECK(cudaGetLastError());
 }
 
 CB_Error_t CA_bench_bine_reduce_cuda(void) {
-    CB_Error_t err = CB_SUCCESS;
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -40,20 +40,21 @@ CB_Error_t CA_bench_bine_reduce_cuda(void) {
     int *recv_buff = NULL;
     int *d_errors  = NULL;
 
-    cudaMalloc((void **)&send_buff, BUFF_SIZE * sizeof(int));
+    CA_CUDA_MALLOC_LABEL(send_buff, BUFF_SIZE * sizeof(int), cleanup);
     CA_bench_reduce_fill_cuda(send_buff, rank, BUFF_SIZE);
 
     if (rank == ROOT)
-        cudaMalloc((void **)&recv_buff, BUFF_SIZE * sizeof(int));
+        CA_CUDA_MALLOC_LABEL(recv_buff, BUFF_SIZE * sizeof(int), cleanup);
 
-    int _ = CA_bine_reduce(send_buff, recv_buff, BUFF_SIZE, MPI_INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
+    CA_bine_reduce(send_buff, recv_buff, BUFF_SIZE, MPI_INT, MPI_SUM, ROOT, MPI_COMM_WORLD);
 
     if (rank == ROOT) {
-        cudaMalloc((void **)&d_errors, sizeof(int));
-        cudaMemset(d_errors, 0, sizeof(int));
+        CA_CUDA_MALLOC_LABEL(d_errors, sizeof(int), cleanup);
+        CA_CUDA_CHECK_LABEL(cudaMemset(d_errors, 0, sizeof(int)), cleanup);
         CA_bench_reduce_check_cuda(recv_buff, size, BUFF_SIZE, d_errors);
         int h_errors = 0;
-        cudaMemcpy(&h_errors, d_errors, sizeof(int), cudaMemcpyDeviceToHost);
+        CA_CUDA_CHECK_LABEL(cudaMemcpy(&h_errors, d_errors, sizeof(int),
+                                       cudaMemcpyDeviceToHost), cleanup);
         if (h_errors) {
             printf("[rank %d] %d mismatches in bine_reduce\n", rank, h_errors);
             MPI_Abort(MPI_COMM_WORLD, 12);
@@ -66,5 +67,5 @@ cleanup:
         cudaFree(recv_buff);
         cudaFree(d_errors);
     }
-    return err;
+    return CB_SUCCESS;
 }
